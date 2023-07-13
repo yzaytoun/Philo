0927995a27b5cccd1bbd70ac8c5d1e8feeee15ad
git@vogsphere-v2.42madrid.com:vogsphere/intra-uuid-32ab929b-05de-4d78-bd4a-2e205f0fd26d-3987085-yzaytoun <yzaytoun@c2r2s3.42madrid.com>
2023-07-13 20:52:24 +0200

    First commit

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: yzaytoun <yzaytoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 17:19:37 by yzaytoun          #+#    #+#             */
-/*   Updated: 2023/07/13 19:04:46 by yzaytoun         ###   ########.fr       */
+/*   Updated: 2023/07/13 20:51:17 by yzaytoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,35 @@
 # define PHILO_H
 
 # include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+/*Macros*/
+# define TRUE 1
+# define FALSE !TRUE
+
+typedef struct s_status
+{
+	int	died;
+	int	eat;
+	int	wait;
+}				t_status;
+
+typedef struct s_mutex
+{
+	pthread	thread;
+}				t_mutex;
+
+typedef struct s_philo
+{
+	t_status	update_status;
+	t_mutex		mutex;
+}				t_philo;
+
+
+/*Functions*/
 
 #endif		/*End Philo.h*/
--- philo/src/printer.c
+++ philo/src/printer.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   printer.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yzaytoun <yzaytoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/13 18:43:09 by yzaytoun          #+#    #+#             */
-/*   Updated: 2023/07/13 20:49:51 by yzaytoun         ###   ########.fr       */
+/*   Created: 2023/07/13 20:52:08 by yzaytoun          #+#    #+#             */
+/*   Updated: 2023/07/13 20:52:09 by yzaytoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
-
-int	main(int argc, char **argv[])
-{
-	if (argc <= 5)
-	{
-		/*create philos*/
-		/**/
-		/*init mutex*/
-		/*timer function*/
-		/*free*/
-	}
-	else
-	{
-		printf("Worng argument number\n");
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
